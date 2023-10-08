0aa31b8f9c75fdec02c1f45e05d549f99fb670e1
Carolina Ortega <carogreen26@gmail.com>
2023-10-08 14:24:08 +0200

    Added interfaces
    Created Article and User

---
+++ src/app/article.ts
@@ -0,0 +1,8 @@
+export interface Article {
+    title: string;
+    subttitle: string;
+    abstract: string;
+    category: string;
+    body: string;
+    image: string; //maybe change to image_media_type, image_data
+}

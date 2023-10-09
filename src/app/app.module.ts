5773044d56afd7a5c1ce8babfe356097f1699fc1
Manuel Hortelano Rodríguez <manuel.hortelano.rodriguez@gmail.com>
2023-10-09 13:51:06 +0200

    Article details component created
    Just a basic template for the article details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NewslistComponent } from './components/newslist/newslist.component';
+import { NewslistComponent } from './newslist/newslist.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NewslistComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  declarations: [AppComponent, NewslistComponent, ArticleDetailsComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
